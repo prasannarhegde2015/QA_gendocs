de8e92fc734eae0ebbd4cb43d3c0eab6f1e3f8ac
prasannarhegde2015 <prasannarhegde@gmail.com>
2021-03-24 11:53:17 +0530

    Update insertstringranom.sql

--- DOCS/SQLBackupScripts/insertstringranom.sql
+++ DOCS/SQLBackupScripts/insertstringranom.sql
@@ -1,15 +1,24 @@
-
+use POP431
 DECLARE @Varcntr int  = 1
 DECLARE @randname varchar(500) 
 DECLARE @index INT
+DECLARE @WellCount int = 358
 
-
-WHILE  (@Varcntr   < 378)
+-- Insert Random GeopgrpahicREgions
+set @Varcntr = 0
+WHILE  (@Varcntr   < @WellCount)
 BEGIN
   SET  @index = CAST(RAND()*(6)+1 AS INT)   
-  Update Well set welGeographicRegion =  CHOOSE( @index ,'Asia','Australia','Africa' ,'Europse','North America','South America') where welPrimaryKey = @Varcntr
+  Update Well set welGeographicRegion =  CHOOSE( @index ,'Asia','Australia','Africa' ,'Europe','North America','South America') where welPrimaryKey = @Varcntr
+  set @Varcntr = @Varcntr +1
+END 
+set @Varcntr = 0
+--Insert Random Engineer Names
+WHILE  (@Varcntr   < @WellCount)
+BEGIN
+  SET  @index = CAST(RAND()*(9)+1 AS INT)   
+  Update Well set welEngineer =  CHOOSE( @index ,'Anit','Hardik','Hima' ,'Prasanna','Pravin','Sudhir','Vandana','Ritu','Swati') where welPrimaryKey = @Varcntr
   set @Varcntr = @Varcntr +1
 END 
 
-
-Select welWellName , welGeographicRegion from Well where welGeographicRegion= 'Africa'
+Select welWellName , welEngineer, welGeographicRegion from Well 
