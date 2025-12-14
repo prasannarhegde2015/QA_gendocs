3537733ef714ac02be40c7344639517151f42616
prasannarhegde2015 <prasannarhegde@gmail.com>
2025-12-14 22:14:36 +0530

    Update serviceList.ps1

--- DOCS/PowershellFunScripts/serviceList.ps1
+++ DOCS/PowershellFunScripts/serviceList.ps1
@@ -12,7 +12,7 @@ foreach($service  in $allservices)
 foreach ($row in $totalrow)
 {
 	
-	if ($row -eq "Stopped" )
+	if ($row.Status -eq "Stopped" )
 	{
 		Write-Host ("{0,-5} {1,-25} {2,-25}" -f $row.SrNo, $row.Name, $row.Status)  -Foregroundcolor red
 	}
