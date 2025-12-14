19444516c6ae9375a9ea6dc5ac48919d95e5971a
prasannarhegde2015 <prasannarhegde@gmail.com>
2025-12-14 22:10:43 +0530

    Create serviceList.ps1

---
+++ DOCS/PowershellFunScripts/serviceList.ps1
@@ -0,0 +1,24 @@
+$allservices = Get-Service | Where-object { $_.Name -Like "*APACHE*" }
+$totalrow = "" | Select-Object SrNo, Name, Status
+$Cnt=1
+foreach($service  in $allservices)
+{
+
+    $totalrow.SrNo = $Cnt
+    $totalrow.Name = $service.Name
+    $totalrow.Status = $service.Status
+	$Cnt=$Cnt+1
+}
+foreach ($row in $totalrow)
+{
+	
+	if ($row -eq "Stopped" )
+	{
+		Write-Host ("{0,-5} {1,-25} {2,-25}" -f $row.SrNo, $row.Name, $row.Status)  -Foregroundcolor red
+	}
+	else
+	{
+		Write-Host ("{0,-5} {1,-25} {2,-25}" -f $row.SrNo, $row.Name, $row.Status)  -Foregroundcolor green
+	}
+	
+}
